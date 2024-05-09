e20911533614626879418d385caadc21b6e314bc
hhmy27 <13163369616@163.com>
2024-05-09 21:12:54 +0800

    refactor: 💡 demo

--- background.ts
+++
@@ -1,3 +0,0 @@
-export {}
-
-console.log("root service worker")
--- src/background/index.ts
+++ src/background/index.ts
@@ -1,18 +1,20 @@
 import "@plasmohq/messaging/background"
 
-console.log("Inner")
+console.log("hello from background/index.ts")
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.contextMenus.create({
-//     id: "openSidePanel",
-//     title: "Open side panel",
-//     contexts: ["all"]
-//   })
-// })
+chrome.runtime.onInstalled.addListener(() => {
+  // BUG: create is undefined
+  chrome.contextMenus.create({
+    id: "openSidePanel",
+    title: "Open side panel",
+    contexts: ["all"]
+  })
+})
 
-// chrome.contextMenus.onClicked.addListener((info, tab) => {
-//   console.log("background open side panel")
-//   if (info.menuItemId === "openSidePanel") {
-//     chrome.sidePanel.open({ windowId: tab.windowId })
-//   }
-// })
+// BUG: onClicked is undefined
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  console.log("background open side panel")
+  if (info.menuItemId === "openSidePanel") {
+    chrome.sidePanel.open({ windowId: tab.windowId })
+  }
+})
